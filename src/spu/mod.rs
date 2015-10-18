5df2f0ad1ffeb15e291c6e7c8ccadeff656f5054
Lionel Flandrin <lionel.flandrin@gmail.com>
2015-10-18 14:26:21 +0200

    Voice registers

--- src/spu/mod.rs
+++ src/spu/mod.rs
@@ -11,11 +11,15 @@ pub struct Spu {
     cd_volume_right: i16,
     ext_volume_left: i16,
     ext_volume_right: i16,
+    /// Last value written to "voice on" register
+    voice_on: (u16, u16),
 
     /// SPU RAM: 256k 16bit samples
     ram: [u16; 256 * 1024],
     /// Write pointer in the SPU RAM
     ram_index: u32,
+
+    voices: [Voice; 24],
 }
 
 impl Spu {
@@ -33,6 +37,7 @@ impl Spu {
 
             ram: [0xbad; 256 * 1024],
             ram_index: 0,
+            voices: [Voice::new(); 24],
         }
     }
 
@@ -43,28 +48,43 @@ impl Spu {
 
         let val = val.as_u16();
 
-        match offset {
-            0x180 => self.main_volume_left = Volume::from_reg(val),
-            0x182 => self.main_volume_right = Volume::from_reg(val),
-            0x184 => self.reverb_volume_left = val as i16,
-            0x186 => self.reverb_volume_right = val as i16,
-            0x18c => self.set_voice_off(val as u32),
-            0x18e => self.set_voice_off((val as u32) << 16),
-            0x190 => self.enable_pitch_modulation(val as u32),
-            0x192 => self.enable_pitch_modulation((val as u32) << 16),
-            0x194 => self.enable_noise_mode(val as u32),
-            0x196 => self.enable_noise_mode((val as u32) << 16),
-            0x198 => self.enable_reverb(val as u32),
-            0x19a => self.enable_reverb((val as u32) << 16),
-            0x1a6 => self.ram_index = (val as u32) << 2,
-            0x1a8 => self.fifo_write(val),
-            0x1aa => self.set_control(val),
-            0x1ac => self.set_ram_control(val),
-            0x1b0 => self.cd_volume_left = val as i16,
-            0x1b2 => self.cd_volume_right = val as i16,
-            0x1b4 => self.ext_volume_left = val as i16,
-            0x1b6 => self.ext_volume_right = val as i16,
-            _ => panic!("Unhandled SPU store {:x} {:04x}", offset, val),
+        if offset < 0x180 {
+            let voice = &mut self.voices[(offset >> 4) as usize];
+
+            match offset & 0xf {
+                0x0 => voice.volume_left = Volume::from_reg(val),
+                0x2 => voice.volume_right = Volume::from_reg(val),
+                0x4 => voice.sample_rate = val,
+                0x6 => voice.start_address = val,
+                0x8 => voice.set_adsr_low(val),
+                0xa => voice.set_adsr_high(val),
+                _ => panic!("Unhandled SPU Voice store {:x} {:04x}",
+                            offset, val),
+            }
+        } else {
+            match offset {
+                0x180 => self.main_volume_left = Volume::from_reg(val),
+                0x182 => self.main_volume_right = Volume::from_reg(val),
+                0x184 => self.reverb_volume_left = val as i16,
+                0x186 => self.reverb_volume_right = val as i16,
+                0x18c => self.set_voice_off(val as u32),
+                0x18e => self.set_voice_off((val as u32) << 16),
+                0x190 => self.enable_pitch_modulation(val as u32),
+                0x192 => self.enable_pitch_modulation((val as u32) << 16),
+                0x194 => self.enable_noise_mode(val as u32),
+                0x196 => self.enable_noise_mode((val as u32) << 16),
+                0x198 => self.enable_reverb(val as u32),
+                0x19a => self.enable_reverb((val as u32) << 16),
+                0x1a6 => self.ram_index = (val as u32) << 2,
+                0x1a8 => self.fifo_write(val),
+                0x1aa => self.set_control(val),
+                0x1ac => self.set_ram_control(val),
+                0x1b0 => self.cd_volume_left = val as i16,
+                0x1b2 => self.cd_volume_right = val as i16,
+                0x1b4 => self.ext_volume_left = val as i16,
+                0x1b6 => self.ext_volume_right = val as i16,
+                _ => panic!("Unhandled SPU store {:x} {:04x}", offset, val),
+            }
         }
     }
 
@@ -75,6 +95,8 @@ impl Spu {
 
         let r =
             match offset {
+                // XXX return previous "voice on" value
+                0x188 => 0,
                 0x1aa => self.control,
                 0x1ae => self.status(),
                 _ => panic!("Unhandled SPU load {:x}", offset),
@@ -130,12 +152,14 @@ impl Spu {
     }
 }
 
+#[derive(Clone, Copy)]
 enum Volume {
     Constant(i16),
     Sweep(SweepConfig),
 }
 
 #[allow(dead_code)]
+#[derive(Clone, Copy)]
 struct SweepConfig {
     /// True if sweep is exponential, otherwise linear
     exponential: bool,
@@ -175,5 +199,37 @@ impl Volume {
 
             Volume::Constant(volume)
         }
+    }
+}
+
+/// State for one of the 24 SPU voices
+#[derive(Clone,Copy)]
+struct Voice {
+    volume_left: Volume,
+    volume_right: Volume,
+    sample_rate: u16,
+    adsr: u32,
+    start_address: u16,
+}
+
+impl Voice {
+    fn new() -> Voice {
+        Voice {
+            volume_left: Volume::new(),
+            volume_right: Volume::new(),
+            sample_rate: 0,
+            adsr: 0,
+            start_address: 0,
+        }
+    }
+
+    fn set_adsr_low(&mut self, val: u16) {
+        self.adsr &= 0xffff0000;
+        self.adsr |= val as u32;
+    }
+
+    fn set_adsr_high(&mut self, val: u16) {
+        self.adsr &= 0xffff;
+        self.adsr |= (val as u32) << 16;
     }
 }
